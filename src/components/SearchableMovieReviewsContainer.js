b95026fc6cdc98702c86d18ebdaf31032b212e77
Rudy Ravelin <rudy@noukod.com>
2019-04-09 00:42:16 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+const NYT_API_KEY = ':)';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
 export default class SearchableMovieReviewsContainer extends React.Component {
+
   constructor() {
     super();
     this.state = {
@@ -15,6 +16,7 @@ export default class SearchableMovieReviewsContainer extends React.Component {
       searchTerm: ''
     }
   }
+
   render() {
     return(
       <div className="searchable-movie-reviews">
